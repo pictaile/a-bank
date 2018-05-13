86a8219f4bed2cfcdefbefeab3639be30fc73ee5
kostya <kostya.ochotnik@gmail.com>
2018-05-14 00:59:53 +0300

    add child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './common/child/child.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+
   ],
   imports: [
     BrowserModule
