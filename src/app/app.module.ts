47cc7b8932593f7158d4117707e68fd3ae56a903
kostiantyn.okhotnyk <kostiantyn.okhotnyk@dev-pro.net>
2018-05-14 14:10:07 +0300

    add counter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './common/child/child.component';
+import { CounterComponent } from './common/counter/counter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ChildComponent,
+    CounterComponent,
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/common/child/child.component.ts
+++ src/app/common/child/child.component.ts
@@ -22,39 +22,38 @@ export class ChildComponent implements OnInit,
         AfterViewChecked,
         AfterViewInit  {
     @Input() name: string;
-    count:number=1;
-     
+    count = 1;
+
     ngOnInit() {
-       
       this.log(`ngOnInit`);
     }
+
     ngOnChanges() {
-       
       this.log(`OnChanges`);
     }
+
     ngDoCheck() {
-       
       this.log(`ngDoCheck`);
     }
+
     ngAfterViewInit() {
-       
       this.log(`ngAfterViewInit`);
     }
+
     ngAfterViewChecked() {
-       
       this.log(`ngAfterViewChecked`);
     }
+
     ngAfterContentInit() {
-       
       this.log(`ngAfterContentInit`);
     }
+
     ngAfterContentChecked() {
-       
       this.log(`ngAfterContentChecked`);
     }
- 
+
     private log(msg: string) {
-        console.log(this.count + ". " + msg);
+        console.log(`${this.count}. ${msg}`);
         this.count++;
     }
 }
